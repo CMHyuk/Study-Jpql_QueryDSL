52386346c294a0ff87c991b73804653d0e05cf13
CMHyuk <alsgur9710@naver.com>
2022-10-25 22:41:06 +0900

    Initial commit

---
+++ src/main/java/com/example/studyjpqlandqurydsl/StudyjpqlandqurydslApplication.java
@@ -0,0 +1,13 @@
+package com.example.studyjpqlandqurydsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyjpqlandqurydslApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyjpqlandqurydslApplication.class, args);
+    }
+
+}
