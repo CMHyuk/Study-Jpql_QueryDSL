84c944b4775b4221396dd54ecda566b66346abe0
CMHyuk <alsgur9710@naver.com>
2022-10-25 22:41:49 +0900

    Member 엔티티

---
+++ src/main/java/com/example/studyjpqlandqurydsl/Member.java
@@ -0,0 +1,22 @@
+package com.example.studyjpqlandqurydsl;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+public class Member {
+
+    @Id @GeneratedValue
+    @Column
+    private Long id;
+    private String name;
+    private int age;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "team_id")
+    private Team team;
+}
