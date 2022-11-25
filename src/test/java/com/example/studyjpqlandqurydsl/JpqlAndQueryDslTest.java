88c778cbbbf1db1b8ca118b606e05e03dfec1fc1
CMHyuk <alsgur9710@naver.com>
2022-11-25 14:48:43 +0900

    비교 추가

--- src/test/java/com/example/studyjpqlandqurydsl/JpqlAndQueryDslTest.java
+++ src/test/java/com/example/studyjpqlandqurydsl/JpqlAndQueryDslTest.java
@@ -1,7 +1,6 @@
 package com.example.studyjpqlandqurydsl;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +11,8 @@ import javax.transaction.Transactional;
 import java.util.List;
 
 import static com.example.studyjpqlandqurydsl.QMember.member;
+import static com.example.studyjpqlandqurydsl.QTeam.team;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Transactional
@@ -31,6 +32,7 @@ public class JpqlAndQueryDslTest {
 
         Member member1 = new Member();
         Member member2 = new Member();
+        Member member3 = new Member();
 
         member1.setName("member1");
         member1.setAge(10);
@@ -38,9 +40,12 @@ public class JpqlAndQueryDslTest {
         member2.setName("member2");
         member2.setAge(20);
         member2.setTeam(team);
+        member3.setName("member3");
+        member3.setAge(20);
 
         em.persist(member1);
         em.persist(member2);
+        em.persist(member3);
     }
 
     @Test
@@ -49,7 +54,7 @@ public class JpqlAndQueryDslTest {
                 .setParameter("name", "member1")
                 .getSingleResult();
 
-        Assertions.assertThat(findMember.getName()).isEqualTo("member1");
+        assertThat(findMember.getName()).isEqualTo("member1");
     }
 
     @Test
@@ -60,8 +65,75 @@ public class JpqlAndQueryDslTest {
                 .where(member.name.eq("member2"))
                 .fetchOne();
 
-        Assertions.assertThat(findMember.getName()).isEqualTo("member2");
+        assertThat(findMember.getName()).isEqualTo("member2");
     }
 
+    @Test
+    void selectMaxAge() {
+        Integer maxAge = em.createQuery("select MAX(m.age) from Member m", Integer.class)
+                .getSingleResult();
 
+        assertThat(maxAge).isSameAs(20);
+    }
+
+    @Test
+    void selectMaxAgeDsl() {
+        Integer maxAge = queryFactory
+                .select(member.age.max())
+                .from(member)
+                .fetchOne();
+
+        assertThat(maxAge).isSameAs(20);
+    }
+
+    @Test
+    void join() {
+        List<Member> findMembers = em.createQuery("select m from Member m join m.team where m.team.teamName =:teamName", Member.class)
+                .setParameter("teamName", "team1")
+                .getResultList();
+
+        for (Member findMember : findMembers) {
+            System.out.println("findMember = " + findMember);
+        }
+    }
+
+    @Test
+    void joinDsl() {
+        List<Member> findMembers = queryFactory
+                .selectFrom(member)
+                .join(member.team, team)
+                .where(team.teamName.eq("team1"))
+                .fetch();
+
+        for (Member findMember : findMembers) {
+            System.out.println("findMember = " + findMember);
+        }
+    }
+
+    @Test
+    void sort() {
+        List<Member> findMembers = em.createQuery("select m from Member m" +
+                        " where m.age = :age order by m.age DESC, m.name DESC", Member.class)
+                .setParameter("age", 20)
+                .getResultList();
+
+        assertThat(findMembers.get(0).getName()).isEqualTo("member3");
+        assertThat(findMembers.get(0).getAge()).isSameAs(20);
+        assertThat(findMembers.get(1).getName()).isEqualTo("member2");
+        assertThat(findMembers.get(1).getAge()).isSameAs(20);
+    }
+
+    @Test
+    void sortDsl() {
+        List<Member> findMembers = queryFactory
+                .selectFrom(member)
+                .where(member.age.eq(20))
+                .orderBy(member.age.desc(), member.name.desc())
+                .fetch();
+
+        assertThat(findMembers.get(0).getName()).isEqualTo("member3");
+        assertThat(findMembers.get(0).getAge()).isSameAs(20);
+        assertThat(findMembers.get(1).getName()).isEqualTo("member2");
+        assertThat(findMembers.get(1).getAge()).isSameAs(20);
+    }
 }
